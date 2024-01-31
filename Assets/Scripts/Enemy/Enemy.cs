27031b0abf28df1103656b9fad8a3e510c5f305c
椿下湫咚 <1772062731lgx@gmail.com>
2024-02-01 00:05:03 +0800

    爬行 小蜘蛛

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    public float flashtime;
+    public GameObject bloodEffect;
+    public GameObject dropCoin;
+    public GameObject floatpoint;
+
+    private SpriteRenderer sr;
+    private Color originalColor;
+    //private PlayerHealth playerHealth;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            Instantiate(dropCoin, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        GameObject gb = Instantiate(floatpoint, transform.position, Quaternion.identity) as GameObject;
+        gb.transform.GetChild(0).GetComponent<TextMesh>().text = damage.ToString();
+        health -= damage;
+        FlashColor(flashtime);
+        Instantiate(bloodEffect, transform.position, Quaternion.identity);
+        //GameController.canShake.Shake();
+    }
+
+    void FlashColor(float time)
+    {
+        sr.color = Color.red;
+        Invoke("ResetColor", time);
+    }
+
+    void ResetColor()
+    {
+        sr.color = originalColor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            //if(playerHealth != null)
+            //{
+            //    playerHealth.DamagePlayer(damage);
+            //}
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemySmartBat.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class EnemySmartBat : Enemy
+{
+    public float speed;
+    public float radius;
+
+    private Transform playerTransform;
+    private bool facingRight = false;  // 假设怪物初始朝向左
+
+    // Start is called before the first frame update
+    public new void Start()
+    {
+        base.Start();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    public new void Update()
+    {
+        base.Update();
+        if (playerTransform != null)
+        {
+            float distance = (transform.position - playerTransform.position).sqrMagnitude;
+
+            if (distance < radius)
+            {
+                Vector2 direction = playerTransform.position - transform.position;
+                if (direction.x > 0 && !facingRight)
+                {
+                    Flip();
+                }
+                else if (direction.x < 0 && facingRight)
+                {
+                    Flip();
+                }
+
+                transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
